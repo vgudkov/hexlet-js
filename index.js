6b9538e80ec082a27aff550a3d643a2988c953a5
Vladimir Gudkov <vgudkov777@gmail.com>
2023-05-10 00:44:12 +0700

    updated JS files after implement of eslint

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-console.log("Hello, Hexlet!");
+import _ from 'lodash';
 
-import _ from "lodash";
+console.log('Hello, Hexlet!');
 
-console.log(_.last(["one", "two"]));
+console.log(_.last(['one', 'two']));
